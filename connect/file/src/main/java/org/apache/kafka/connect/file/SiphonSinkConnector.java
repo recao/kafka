c2e5943f9dba554c096c5f39be7905af82349abd
Ren Cao <recao@microsoft.com>
2017-08-03 20:52:41 -0700

    small fixes

--- connect/file/src/main/java/org/apache/kafka/connect/file/SiphonSinkConnector.java
+++ connect/file/src/main/java/org/apache/kafka/connect/file/SiphonSinkConnector.java
@@ -1,8 +1,6 @@
 package org.apache.kafka.connect.file;
 
 import org.apache.kafka.common.config.ConfigDef;
-import org.apache.kafka.common.config.ConfigDef.Importance;
-import org.apache.kafka.common.config.ConfigDef.Type;
 import org.apache.kafka.common.utils.AppInfoParser;
 import org.apache.kafka.connect.connector.Task;
 import org.apache.kafka.connect.sink.SinkConnector;
@@ -12,10 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Very simple connector that works with the console. This connector supports both source and
- * sink modes via its 'mode' setting.
- */
+
 public class SiphonSinkConnector extends SinkConnector {
 
     @Override
@@ -30,12 +25,15 @@ public class SiphonSinkConnector extends SinkConnector {
 
     @Override
     public Class<? extends Task> taskClass() {
-        return FileStreamSinkTask.class;
+        return SiphonSinkTask.class;
     }
 
     @Override
     public List<Map<String, String>> taskConfigs(int maxTasks) {
         ArrayList<Map<String, String>> configs = new ArrayList<>();
+        for(int i = 0; i < maxTasks; i++) {
+            configs.add(new HashMap<String, String>());
+        }
         return configs;
     }
 
--- connect/file/src/main/java/org/apache/kafka/connect/file/SiphonSinkTask.java
+++ connect/file/src/main/java/org/apache/kafka/connect/file/SiphonSinkTask.java
@@ -2,17 +2,8 @@ package org.apache.kafka.connect.file;
 
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Map;
 
